aa456b8925d5e9957e64e970ce81da61c1908501
Anastasia Lenina <anastasia.lenina@applifting.cz>
2025-01-04 12:02:57 +0100

    delete old Network Layer

--- Pokedex/Networking/APIManager/NetworkingError.swift
+++ Pokedex/Networking/APIManager/NetworkingError.swift
@@ -2,33 +2,50 @@
 //  NetworkingError.swift
 //  Pokedex
 //
-//  Created by Anastasia Lenina on 27.12.2024.
+//  Created by Anastasia Lenina on 03.11.2023.
 //
 
 import Foundation
-//
-// enum NetworkingError: Error {
-//    case apiError
-//    case apiErrorWithMessage(String)
-//    case invalidEndpoint
-//    case invalidResponse
-//    case invalidUrlRequest // using
-//    case noData
-//    case other(Error)
-//
-//    var description: String {
-//        switch self {
-//        case .apiError: return LocalizedString.Errors.unexpectedErrorOccurred
-//        case let .apiErrorWithMessage(message): return message
-//        case .invalidEndpoint: return "#invalidEndpoint"
-//        case .invalidResponse: return "#invalidResponse"
-//        case .invalidUrlRequest: return "#invalidURLRequest"
-//        case .noData: return "#noData"
-//        case let .other(err): return err.localizedDescription
-//        }
-//    }
-//
-//    static func map(_ error: Error) -> NetworkingError {
-//        (error as? NetworkingError) ?? .other(error)
-//    }
-// }
+
+enum NetworkingError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case genericError
+    case networkConnection
+    case unknown(_ error: Error)
+
+    var localizedDescription: ErrorDescription {
+        switch self {
+        case .genericError:
+            return ErrorDescription(L.Errors.genericTitle, L.Errors.genericMessage)
+        case .invalidURL:
+            return ErrorDescription(L.Errors.invalidUrlTile, L.Errors.invalidUrlMessage)
+        case .invalidData:
+            return ErrorDescription(L.Errors.invalidDataTile, L.Errors.invalidDataMessage)
+        case .invalidResponse:
+            return ErrorDescription(L.Errors.invalidResponseTile, L.Errors.invalidResponseMessage)
+        case .networkConnection:
+            return ErrorDescription(
+                LocalizedString.InternetConnection.connectionError,
+                LocalizedString.InternetConnection.title + " " + LocalizedString.InternetConnection.description
+            )
+        case .unknown:
+            return ErrorDescription(L.Errors.unknownTile, L.Errors.unknownMessage)
+        }
+    }
+
+    struct ErrorDescription {
+        let title: String
+        let message: String
+
+        init(_ title: String, _ message: String) {
+            self.title = title
+            self.message = message
+        }
+    }
+
+    static func map(_ error: Error) -> NetworkingError {
+        (error as? NetworkingError) ?? .unknown(error)
+    }
+}
